a423b9d0220f842c00fc806120863a3f13bcb92a
vibhashan <ranhindavibhashana@gmail.com>
2025-02-15 13:11:47 +0530

    chore: initial commit

---
+++ src/main/java/org/vibhashana/jakarta/hello/Hello.java
@@ -0,0 +1,14 @@
+package org.vibhashana.jakarta.hello;
+
+public class Hello {
+
+	private String name;
+	
+	public Hello(String name) {
+        this.name = name;
+	}
+
+	public String getHello(){
+		return name;
+	}
+}
